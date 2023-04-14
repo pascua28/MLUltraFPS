4bc715cfcfbf9e75b22ea6339cc12585acb56a10
Samuel Pascua <sgpascua@ngcp.ph>
2023-04-14 20:40:37 +0800

    Simplify and optimize code
    Signed-off-by: Samuel Pascua <sgpascua@ngcp.ph>

--- app/src/main/java/com/sammy/spoofer/Spoofer.java
+++ app/src/main/java/com/sammy/spoofer/Spoofer.java
@@ -1,57 +1,29 @@
 package com.sammy.spoofer;
-import android.util.Log;
-
-import java.util.HashMap;
-import java.util.Map;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
-import de.robv.android.xposed.XC_MethodHook;
-import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 public class Spoofer implements IXposedHookLoadPackage {
-    private static final Map<String, String> FAKE_VARS = new HashMap<>();
 
-    static {
-        FAKE_VARS.put("ro.product.manufacturer", "Xiaomi");
-        FAKE_VARS.put("ro.product.brand", "Xiaomi");
-        FAKE_VARS.put("ro.product.name", "Xiaomi");
-    }
+    private static final String TARGET_PACKAGE = "com.mobile.legends";
+    private static final String MANUFACTURER = "Xiaomi";
+    private static final String BRAND = "Xiaomi";
+    private static final String DEVICE_NAME = "Xiaomi";
+    private static final String MODEL = "M2102J20SG";
+    private static final String PRODUCT = "vayu_global";
+    private static final String BOARD = "vayu";
 
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
-        try {
-            XposedBridge.hookAllMethods(XposedHelpers.findClass("android.os.SystemProperties", lpparam.classLoader),
-                    "get", new XC_MethodHook() {
-                        @Override
-                        protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                            if (FAKE_VARS.containsKey(param.args[0].toString())) {
-                                param.setResult(FAKE_VARS.get(param.args[0].toString()));
-                            }
-                        }
-                    });
+        if (lpparam.packageName.equals(TARGET_PACKAGE)) {
 
-            XposedBridge.hookAllMethods(XposedHelpers.findClass("android.os.SystemProperties", lpparam.classLoader),
-                    "native_get", new XC_MethodHook() {
-                        @Override
-                        protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                            if (FAKE_VARS.containsKey(param.args[0].toString())) {
-                                param.setResult(FAKE_VARS.get(param.args[0].toString()));
-                            }
-                        }
-                    });
-
-
-            XposedHelpers.setStaticObjectField(android.os.Build.class, "MODEL", "M2102J20SG");
-            XposedHelpers.setStaticObjectField(android.os.Build.class, "MANUFACTURER", "Xiaomi");
-            XposedHelpers.setStaticObjectField(android.os.Build.class, "PRODUCT", "vayu_global");
-            XposedHelpers.setStaticObjectField(android.os.Build.class, "BOARD", "vayu");
-
-            Log.d("Sammy: ", "build");
-
-        } catch (Throwable throwable) {
-            Log.d("Sammy: ", "error");
+            XposedHelpers.setStaticObjectField(android.os.Build.class, "MANUFACTURER", MANUFACTURER);
+            XposedHelpers.setStaticObjectField(android.os.Build.class, "BRAND", BRAND);
+            XposedHelpers.setStaticObjectField(android.os.Build.class, "DEVICE", DEVICE_NAME);
+            XposedHelpers.setStaticObjectField(android.os.Build.class, "MODEL", MODEL);
+            XposedHelpers.setStaticObjectField(android.os.Build.class, "PRODUCT", PRODUCT);
+            XposedHelpers.setStaticObjectField(android.os.Build.class, "BOARD", BOARD);
         }
     }
 }
